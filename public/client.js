b3ecfe1eac0ff4bbe1c0c76c89fed03d200826c7
Nicolas <nicolas.merinian@gmail.com>
2017-04-01 20:10:43 +0200

    set up messages sending

--- public/client.js
+++ public/client.js
@@ -1,1 +1,16 @@
 var socket = io();
+
+$('form').submit(function(e) {
+    e.preventDefault();
+
+    var message = {
+        text : $('#m').val()
+    };
+	
+    if (message.text.trim().length !== 0) {
+      socket.emit('chat-message', message);
+    }
+	
+    $('#m').val(''); 
+    $('#chat input').focus();
+});
--- server.js
+++ server.js
@@ -13,6 +13,15 @@ http.listen(3000, function(){
   console.log('Server is listening on *:3000');
 });
 
-io.on('connection', function(socket){
+io.on('connection', function(socket) {
+	
   console.log('a user connected');
+  socket.on('disconnect', function() {
+    console.log('user disconnected');
+  });
+  
+  socket.on('chat-message', function (message) {
+    console.log('message : ' + message.text);
+  });
+  
 });
